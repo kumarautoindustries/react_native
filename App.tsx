ab13dd348a0d7f603452959fd1bc3e4e3e1d52e6
AvdheshNigam <kumarnigam28@gmail.com>
2024-05-17 16:32:29 +0530

    Layout

--- App.tsx
+++ App.tsx
@@ -1,15 +1,6 @@
 import * as React from 'react';
 import { Button, Text, View } from 'react-native';
 import { NavigationContainer, useNavigation, DrawerActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
-import HomeScreen from './components/screens/HomeScreen';
-import SettingScreen from './components/screens/SetttingScreen';
-import UserScreen from './components/screens/UserScreen';
-import NotificationsScreen from './components/screens/NotificationsScreen';
-
-import Icon from 'react-native-vector-icons/Entypo';
 import DrawerNavigator from './components/navigation/DrawerNavigator';
 
 const App = () => {
