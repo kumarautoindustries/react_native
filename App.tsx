8a6643021df79021de80c5e026ab34c625e408b1
AvdheshNigam <kumarnigam28@gmail.com>
2024-05-17 16:33:07 +0530

    Layout

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Button, Text, View } from 'react-native';
-import { NavigationContainer, useNavigation, DrawerActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './components/navigation/DrawerNavigator';
 
 const App = () => {
