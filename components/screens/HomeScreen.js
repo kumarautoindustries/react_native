c931c54730655156c60cb98dbe760bd56868760a
AvdheshNigam <kumarnigam28@gmail.com>
2024-05-18 11:52:37 +0530

    Screen

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
+import { fonts } from '../../src/utils/fonts';
+
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications 5"
-      />
-    </View>
+    <>
+      <Text style={{ fontFamily: fonts.SemiBold }}>Welcome to my home screen</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+        <Button
+          onPress={() => navigation.navigate('Notifications')}
+          title="Go to notifications 5"
+        />
+      </View>
+    </>
   );
 }
 
